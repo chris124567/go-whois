6e175f268eca87893c3c4b768b537b1da9697e59
Chris <>
2019-11-09 22:56:54 -0500

    fix formatting

--- pkg/whoisclient/parse.go
+++ pkg/whoisclient/parse.go
@@ -11,7 +11,7 @@ func tld_split(domain_name string) string { // get last split of '.', which shou
 	return ss[len(ss)-1]
 }
 
-func Standardize_Domain_Name(unformatted_domain_name string) string { // 	// standardize domain name: remove subdomains, lower case everything
+func Standardize_Domain_Name(unformatted_domain_name string) string { // standardize domain name: remove subdomains, lower case everything
 	proper_domain, err := publicsuffix.EffectiveTLDPlusOne(strings.ToLower(unformatted_domain_name))
 	if err != nil {
 		log.Printf("Failed to parse domain name %s", unformatted_domain_name)
